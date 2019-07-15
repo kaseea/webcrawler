a06a177455303022d7813a8831664ffa7f53ae9d
K <774162+kaseea@users.noreply.github.com>
2019-07-15 12:44:27 -0700

    problem with set timeout

--- crawl-dockets.js
+++ crawl-dockets.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 let docketCrawlCount = 0;
 
-const loadPage = function loadPage(url) {
+const loadPage = function loadPage(url, county) {
     request(url, 
         { strictSSL: false, 
             headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
@@ -19,26 +19,54 @@ const loadPage = function loadPage(url) {
                     if ($(this).find(':nth-child(6)').text()) {
                         let description = $(this).find(':nth-child(3)').text();
                         let fee = $(this).find(':nth-child(6)').text();
-                        let county = element + '.txt'; 
-                        fs.appendFileSync(county, description + ',' + fee + '\n');
+                        fee = fee.substring(1, fee.length)
+                        let countyFile = 'fees-by-county/' + county + '.txt'; 
+                        fs.appendFileSync(countyFile, description + ',' + fee + '\n');
                     }
                 })
             } else {
-                console.log("We’ve encountered an error: " + error);
+                console.log("We’ve encountered an error in loadPage: " + error);
             }
         }
       );
 }
-const crawlPage = function crawlPage(pages) {
-    pages.forEach(function(element) {
-        let url = `https://www.oscn.net/dockets/` + element;
 
-    })
+
+const crawlPage = function crawlPage() {
+    let county = 'adair';
+    fs.readFile('links-by-county/' + county + '.txt', 'utf8', function (err, data) {
+        if (!err) {
+        const dataArray = data.split(/\r?\n/);
+        const betterLink = dataArray.map((link) => 
+           link.substring(0, link.length - 1)
+        );
+        const splicedLink = betterLink.splice(0,20);
+        console.log('docket links to be anaylzed: ' + splicedLink.length);
+        splicedLink.forEach(function(link) {
+            setTimeout(loadPage(link, county), 2000)
+        })
+        } else {
+            console.log('error for crawlPage: ' + err);
+        }
+      });
+    // txtFile.forEach(function(link) {
+        // setTimeout(loadPage(link, element), 2000);
+        
+    // })
 }
 
-function myFunction() {
-    // your code to run after the timeout
-}
 
-// stop for sometime if needed
-setTimeout(myFunction, 5000);
+crawlPage()
+// function myFunction() {
+//     // your code to run after the timeout
+// }
+
+// // stop for sometime if needed
+// setTimeout(myFunction, 5000);
+
+
+let myGreeting = setTimeout(function sayHi() {
+    // crawlPage(distinctLinks);
+    console.log('ddddddddddd dockets crawled: ' + docketCrawlCount);
+
+  }, 100000)
